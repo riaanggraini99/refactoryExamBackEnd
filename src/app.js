26ad4c3b727c8e38ed1ac6ab060171f61e247992
Ria Anggraini <35605707+riaanggraini99@users.noreply.github.com>
2018-03-27 14:40:46 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,51 +1,24 @@
-const path = require('path')
-const express = require('express')
-const logger = require('morgan')
+const path = require('path');
+
+const express = require('express');
+const logger = require('morgan');
 const bodyParser = require('body-parser');
 
 const app = express();
 
-const indexRoutes = require('./routes/index.js')
+const indexRoutes = require('./routes/index');
 
-app.set('port',process.env.PORT || 3000);
-app.set('views', path.join(__dirname,'views'))
-app.set('view engine','ejs');
+// settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
+// middlewares
 app.use(logger('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
-
-
-//route
+app.use(bodyParser.urlencoded({extended: false}))
+// routes
 app.use('/', indexRoutes);
 
-
-
-app.listen(app.get('port'),() =>{
-console.log('server on port', app.get('port'));
+app.listen(app.get('port'), () => {
+  console.log(`server on port ${app.get('port')}`);
 });
-
-
-/*const logger = require('morgan')
-const mongoose=require('mongoose');
-
-const bodyParser = require('body-parser')
-
-const app = express();
-
-const indexRoutes = require('')
-
-//setting
-
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(_dirname,'views'));
-app.set('view engine','ejs');
-
-//middlewares
-app.use('logger'('dev'));
-app.use(bodyParser.urlendcoded({extended: false}));
-
-app.use('/',indexRoutes);
-
-app.listen(app.get('port'),()=>{
-    console.log('listen on port', app.get('port'));
-}); */
